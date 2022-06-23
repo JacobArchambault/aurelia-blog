c3d10ec2b96318cac85d7a2c3674226147ff40a6
Jacob Archambault <Jacob.Archambault@venminder.com>
2022-06-23 17:39:49 -0400

    Fix routing titles

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ import { PLATFORM } from "aurelia-framework";
   
 export class App {
   configureRouter(config: RouterConfiguration, router: Router): void {
-    config.title = 'Routing demo';
+    config.title = "Creed's blog";
     config.options.pushState = true;
     config.map([
-      { route: ['', '/posts'],   moduleId: PLATFORM.moduleName('elements/posts/posts'), title: "Creed's blog" },
-      { route: '/posts/1',   moduleId: PLATFORM.moduleName('elements/post-detail/post-detail'), title: "Creed's blog" },
+      { route: ['', '/posts'],   moduleId: PLATFORM.moduleName('elements/posts/posts'), title: "posts" },
+      { route: '/posts/1',   moduleId: PLATFORM.moduleName('elements/post-detail/post-detail'), title: "post" },
     ]);
   }
 }
