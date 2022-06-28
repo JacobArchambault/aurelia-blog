015c479bdce38028963afbc3edefed67f51fbb1f
Jacob Archambault <Jacob.Archambault@venminder.com>
2022-06-28 11:34:58 -0400

    remove empty index.ts file and just use static html page

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ export class App {
     config.title = "Creed's blog";
     config.options.pushState = true;
     config.map([
-      { route: ['',], moduleId: PLATFORM.moduleName('index'), title: "Home" },
+      { route: ['',], moduleId: PLATFORM.moduleName('index.html'), title: "Home" },
       { route: ['/posts'], moduleId: PLATFORM.moduleName('elements/posts/posts'), title: "Posts" },
       { route: '/posts/:id', moduleId: PLATFORM.moduleName('elements/post-detail/post-detail'), title: "Post", name: 'posts' },
     ]);
