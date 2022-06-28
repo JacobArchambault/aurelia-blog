87adf15f55887df145b5dd6f93f60f6eb1285718
Jacob Archambault <Jacob.Archambault@venminder.com>
2022-06-28 16:26:44 -0400

    Add comments to views for individual posts

--- src/elements/post-detail/post-detail.ts
+++ src/elements/post-detail/post-detail.ts
@@ -1,15 +1,19 @@
-import { inject } from 'aurelia-framework';
+import { autoinject } from 'aurelia-framework';
+import { CommentsService } from './../../services/comments-service';
 import { Post } from 'interfaces/post';
 import { PostsService } from 'services/posts-service';
-
-@inject(PostsService)
+import { Comment } from 'interfaces/comment';
+@autoinject
 export class PostDetail {
     private post: Post;
+    private comments: Comment[];
 
-    constructor(private postsService: PostsService) { }
+    constructor(private postsService: PostsService, private commentsService: CommentsService) { }
 
     private async activate(params): Promise<void> {
         await this.postsService.getById(params.id)
             .then(post=> this.post = post);
+        await this.commentsService.getByPostId(params.id)
+        .then(comments => this.comments = comments);
     }
 }
